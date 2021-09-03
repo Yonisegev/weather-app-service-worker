2beb58bb708abe01ecc1203d2f4a4a1be229131d
Yonisegev <yonisegev6@gmail.com>
2021-09-03 20:08:44 +0300

    deployed on firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,18 @@ import { WeatherComponent } from './components/weather/weather.component';
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment';
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +30,8 @@ import { environment } from '../environments/environment'
     MatIconModule,
     MatInputModule,
     MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       registrationStrategy: 'registerImmediately'
